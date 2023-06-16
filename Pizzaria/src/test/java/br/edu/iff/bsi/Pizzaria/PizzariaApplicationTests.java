e7bda8f9acd2f0bcec0c5a943713d582464376df
mbrag <mbrag@Matheus-Braga>
2023-06-15 21:39:26 -0300

    FIRST COMMIT

---
+++ Pizzaria/src/test/java/br/edu/iff/bsi/Pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.bsi.Pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
