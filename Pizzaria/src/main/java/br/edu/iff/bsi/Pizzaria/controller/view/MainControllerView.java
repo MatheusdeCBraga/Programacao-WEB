ed5144984d243907ce4465df5d22f4a8d47b0fef
mbrag <mbrag@Matheus-Braga>
2023-06-22 19:00:21 -0300

    Commit HTML

---
+++ Pizzaria/src/main/java/br/edu/iff/bsi/Pizzaria/controller/view/MainControllerView.java
@@ -0,0 +1,16 @@
+package br.edu.iff.bsi.Pizzaria.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/home")
+public class MainControllerView {
+
+	@GetMapping()
+	public String page() {
+		return "index";
+	}
+
+}
